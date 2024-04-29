b722532d288a2223124690532cbe58a0e305c56a
IvanRadovan <ivano-radovanovic@hotmail.com>
2024-04-29 16:46:50 +0200

    HATEOAS Removed

--- src/main/java/com/example/roombooking/models/RoomType.java
+++ src/main/java/com/example/roombooking/models/RoomType.java
@@ -5,8 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
 
-import java.util.ArrayList;
-
 @Entity
 @Data
 @Builder
@@ -20,24 +18,8 @@ public class RoomType {
     private String type;
     private int extraBeds;
 
-//    @OneToMany(mappedBy = "roomType")
-//    private List<Room> rooms;
-
     public RoomType(String type, int extraBeds) {
         this.type = type;
         this.extraBeds = extraBeds;
-//        this.rooms = new ArrayList<>();
     }
-
-//    public void addRoom(Room room) {
-//        boolean notFound = rooms.stream().noneMatch(it -> it.equals(room));
-//        if (notFound) {
-//            rooms.add(room);
-//        }
-//    }
-//
-//    public void removeRoom(Room booking) {
-//        rooms.removeIf(it -> it.equals(booking));
-//    }
-
 }
