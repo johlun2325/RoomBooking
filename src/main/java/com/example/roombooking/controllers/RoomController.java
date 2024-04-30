6bce26c7e8849b067ca6bded3984787b96d19601
IvanRadovan <ivan_radovan@outlook.com>
2024-04-30 13:43:56 +0200

    Working on room search

--- src/main/java/com/example/roombooking/controllers/RoomController.java
+++ src/main/java/com/example/roombooking/controllers/RoomController.java
@@ -3,9 +3,11 @@ package com.example.roombooking.controllers;
 import com.example.roombooking.dto.RoomLiteDTO;
 import com.example.roombooking.services.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/room")
@@ -25,11 +27,25 @@ public class RoomController {
     }
 
     // http://localhost:8080/room/search?startDate=2024-01-10&endDate=2024-01-15&numberOfPeople=1
+//    @GetMapping("/search")
+//    public List<RoomLiteDTO> searchRooms(@RequestParam String startDate,
+//                                          @RequestParam String endDate,
+//                                          @RequestParam int numberOfPeople) {
+//        return roomService.searchAvailableRooms(startDate, endDate, numberOfPeople);
+//    }
+
+    // http://localhost:8080/room/search?startDate=2024-01-10&endDate=2024-01-15&numberOfPeople=1
     @GetMapping("/search")
-    public List<RoomLiteDTO> searchRooms(@RequestParam String startDate,
-                                          @RequestParam String endDate,
-                                          @RequestParam int numberOfPeople) {
-        return roomService.searchAvailableRooms(startDate, endDate, numberOfPeople);
+    public String searchRooms(@RequestParam String startDate,
+                              @RequestParam String endDate,
+                              @RequestParam int numberOfPeople,
+                              Model model) {
+        model.addAllAttributes(Map.of(
+                "startDate", startDate,
+                "endDate", endDate,
+                "numberOfPeople", numberOfPeople));
+
+        return "searchForm";
     }
 
 }
