269e5712d0a3dc8ae311bc0f95462a5bf290ab7d
Johanna Lundström <johanna.ml.lundstrom@gmail.com>
2024-05-01 14:09:36 +0200

    changed bean.

--- src/main/java/com/example/roombooking/RoomBookingApplication.java
+++ src/main/java/com/example/roombooking/RoomBookingApplication.java
@@ -1,7 +1,21 @@
 package com.example.roombooking;
 
+import com.example.roombooking.models.Booking;
+import com.example.roombooking.models.Customer;
+import com.example.roombooking.models.Room;
+import com.example.roombooking.models.RoomType;
+import com.example.roombooking.repos.BookingRepo;
+import com.example.roombooking.repos.CustomerRepo;
+import com.example.roombooking.repos.RoomRepo;
+import com.example.roombooking.repos.TypeRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 public class RoomBookingApplication {
@@ -44,9 +58,9 @@ public class RoomBookingApplication {
 //            Booking booking1 = new Booking(customer1, room1, 1, LocalDate.of(2024, 1, 10), LocalDate.of(2024, 1, 15));
 //            Booking booking2 = new Booking(customer2, room2, 2, LocalDate.of(2024, 2, 20), LocalDate.of(2024, 2, 25));
 //            Booking booking3 = new Booking(customer3, room3, 3, LocalDate.of(2024, 2, 15), LocalDate.of(2024, 2, 20));
-//            Booking booking4 = new Booking(customer4, room1, 2, LocalDate.of(2024, 2, 5), LocalDate.of(2024, 2, 7));
+////            Booking booking4 = new Booking(customer4, room1, 2, LocalDate.of(2024, 2, 5), LocalDate.of(2024, 2, 7));
 //
-//            bookingRepo.saveAll(List.of(booking1, booking2, booking3, booking4));
+//            bookingRepo.saveAll(List.of(booking1, booking2, booking3));
 //        };
 //    }
 
