aa6581d821527b516224c0f6c00f6a2f4b7274e2
IvanRadovan <ivano-radovanovic@hotmail.com>
2024-05-08 22:37:19 +0200

    Add a hyperlink on each row for every customer

--- src/main/java/com/example/roombooking/controllers/BusinessCustomerController.java
+++ src/main/java/com/example/roombooking/controllers/BusinessCustomerController.java
@@ -1,10 +1,13 @@
 package com.example.roombooking.controllers;
 
 
+import com.example.roombooking.dto.BookingDTO;
 import com.example.roombooking.dto.BusinessCustomerDTO;
 import com.example.roombooking.models.BusinessCustomer;
 import com.example.roombooking.services.BusinessCustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/businessCustomer")
 @RequiredArgsConstructor
 public class BusinessCustomerController {
@@ -20,13 +23,31 @@ public class BusinessCustomerController {
     private final BusinessCustomerService businessCustomerService;
 
     @GetMapping()
-    List<BusinessCustomerDTO> getAllContractCustomers() {
-        return businessCustomerService.findAllContractCustomers();
+    public String getAllBusinessCustomers(Model model) {
+        List<BusinessCustomerDTO> all = businessCustomerService.findAllContractCustomers();
+        model.addAttribute("allBusinessCustomers", all);
+        model.addAttribute("pageHeader", "Företagskunder");
+        model.addAttribute("header", "Alla företagskunder");
+        model.addAttribute("businessCustomerId", "Företagskund-ID");
+        model.addAttribute("companyName", "Företagsnamn");
+        model.addAttribute("customerName", "Kundnamn");
+        model.addAttribute("country", "Land");
+
+        return "all-business-customers";
     }
 
     @GetMapping({"/{id}"})
-    BusinessCustomerDTO getContractCustomer(@PathVariable Long id) {
-        return businessCustomerService.findContractCustomerById(id);
+    String getBusinessCustomer(@PathVariable Long id, Model model) {
+        BusinessCustomerDTO businessCustomer = businessCustomerService.findContractCustomerById(id);
+        model.addAttribute("businessCustomers", businessCustomer);
+        model.addAttribute("pageHeader", "Företagskunder");
+        model.addAttribute("header", "Företagskund");
+        model.addAttribute("businessCustomerId", "Företagskund-ID");
+        model.addAttribute("companyName", "Företagsnamn");
+        model.addAttribute("customerName", "Kundnamn");
+        model.addAttribute("country", "Land");
+
+        return "show-business-customer-info";
     }
 
 }
