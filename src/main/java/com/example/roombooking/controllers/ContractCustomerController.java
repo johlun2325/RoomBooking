b4977e4a67e39f9e7ac01befc9b0f04fb3b736ef
IvanRadovan <ivan_radovan@outlook.com>
2024-05-10 14:55:25 +0200

    HTML for search and filter implemented.
    Working on functionality... no success :(

--- src/main/java/com/example/roombooking/controllers/ContractCustomerController.java
+++ src/main/java/com/example/roombooking/controllers/ContractCustomerController.java
@@ -2,13 +2,18 @@ package com.example.roombooking.controllers;
 
 
 import com.example.roombooking.dto.ContractCustomerDTO;
+import com.example.roombooking.repos.ContractCustomerRepo;
 import com.example.roombooking.services.ContractCustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,6 +23,9 @@ import java.util.List;
 public class ContractCustomerController {
 
     private final ContractCustomerService contractCustomerService;
+
+    // Ska egen inte vara här (vi måste använda DTO)
+    private final ContractCustomerRepo contractCustomerRepo;
 
     @GetMapping("/all")
     public String getAllContractCustomers(Model model) {
@@ -33,7 +41,7 @@ public class ContractCustomerController {
     }
 
     @GetMapping({"/{id}"})
-    String getContractCustomer(@PathVariable Long id, Model model) {
+    public String getContractCustomer(@PathVariable Long id, Model model) {
         ContractCustomerDTO contractCustomer = contractCustomerService.findContractCustomerById(id);
         model.addAttribute("contractCustomer", contractCustomer);
         model.addAttribute("pageHeader", "Företagskunder");
@@ -49,6 +57,31 @@ public class ContractCustomerController {
         model.addAttribute("fax", "Fax");
 
         return "show-contract-customer-info";
+    }
+
+    @GetMapping({})
+    public String sort(Model model,
+                       @RequestParam(defaultValue = "1") int pageNo,
+                       @RequestParam(defaultValue = "ID") int pageSize,
+                       @RequestParam(defaultValue = "name") String sortCol,
+                       @RequestParam(defaultValue = "ASC") String sortOrder,
+                       @RequestParam(defaultValue = "") String  q) {
+
+        q = q.trim();
+
+        Sort sort = Sort.by(Sort.Direction.fromString(sortOrder), sortCol);
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+        if (!q.isEmpty()) {
+            model.addAttribute("contractCustomers", contractCustomerRepo.findAllByContactNameContaining(q, sort));
+            model.addAttribute("totalPages", 1);
+            model.addAttribute("pageNo", 1);
+        } else {
+            var list = contractCustomerRepo.findAllBy(sort);
+            model.addAttribute("pageNo", pageNo);
+            model.addAttribute("contractCustomers", list);
+        }
+
+        return "redirect:how-contract-customer-info";
     }
 
 }
--- src/main/java/com/example/roombooking/repos/ContractCustomerRepo.java
+++ src/main/java/com/example/roombooking/repos/ContractCustomerRepo.java
@@ -1,10 +1,18 @@
 package com.example.roombooking.repos;
 
 import com.example.roombooking.models.ContractCustomer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContractCustomerRepo extends JpaRepository<ContractCustomer, Long> {
+
+
+    List<ContractCustomer> findAllByContactNameContaining(String contractName, Sort sort);
+
+    List<ContractCustomer> findAllBy(Sort sort);
 
 }
