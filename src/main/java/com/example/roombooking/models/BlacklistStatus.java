d9269ae47e25feec9d36e1bc4ec21938f4cbe2bf
Johanna Lundström <johanna.ml.lundstrom@gmail.com>
2024-05-17 11:42:43 +0200

    Cleanup

--- src/main/java/com/example/roombooking/models/BlacklistStatus.java
+++ src/main/java/com/example/roombooking/models/BlacklistStatus.java
@@ -2,12 +2,10 @@ package com.example.roombooking.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BlacklistStatus {
--- src/main/java/com/example/roombooking/models/BlacklistedCustomer.java
+++ src/main/java/com/example/roombooking/models/BlacklistedCustomer.java
@@ -1,9 +1,6 @@
 package com.example.roombooking.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
