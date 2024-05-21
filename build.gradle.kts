b74b23d027b5841f4f1a4e3255ca5e8ce4ab8f3a
Johanna Lundström <johanna.ml.lundstrom@gmail.com>
2024-05-21 15:24:12 +0200

    added dependency for h2 database in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    //testImpl h2 - se backend3start
+    testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
