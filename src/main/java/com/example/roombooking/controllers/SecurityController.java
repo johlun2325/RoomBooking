11b98ae6ab960d97f78c91df09edf79365451665
IvanRadovan <ivano-radovanovic@hotmail.com>
2024-05-31 14:56:48 +0200

    Plain text in the confirmation booking HTML can now be updated by admin.

--- src/main/java/com/example/roombooking/controllers/SecurityController.java
+++ src/main/java/com/example/roombooking/controllers/SecurityController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Controller
